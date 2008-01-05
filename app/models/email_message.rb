963d27fcd979c1151996d2716a02b7b51a83a9aa
jkay <jkay@fc833d56-6535-0410-917c-849625c20969>
2008-01-05 21:27:10 +0000

    adding different email validation to the email message
    git-svn-id: svn+ssh://208.53.44.52/usr/local/svn/mydaddypuzzles@45 fc833d56-6535-0410-917c-849625c20969

--- app/models/email_message.rb
+++ app/models/email_message.rb
@@ -1,6 +1,6 @@
 class EmailMessage < ActiveRecord::Base
   validates_presence_of :message, :subject, :name
   validates_format_of :from_email,
-                      :with => %r{^([ a-z0-9])+@([ a-z0-9_-])+(\.[a-zA-Z]+)$}i,
+                      :with => %r{^([ a-z0-9_\.-])+@([ a-z0-9_-])+([\. a-z0-9_-])*(\.[a-zA-Z]+)$}i,
 		      		        :message => "Doesn't look like a valid email address"
 end
