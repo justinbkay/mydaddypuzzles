32e06f643fb21e227735df3404c540f7cb6a31fc
Justin <justinbkay@gmail.com>
2013-02-15 14:39:54 -0700

    Can now see the cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -14,4 +14,18 @@ class Cart < ActiveRecord::Base
     self.cart_items.map(&:price).reduce(&:+)
   end
 
+  def shipping_cost
+    # if total items is odd lets add 1
+    return 0 unless @shipping == '1'
+
+    # if usps is shipping we return calc'd shipping
+    ti = cart_items.size
+    ti = ti % 2 == 1 ? ti += 1 : ti
+    if ti < 3
+      8
+    else
+      ((ti - 2) * 2) + 8
+    end
+  end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,10 @@
 class CartItem < ActiveRecord::Base
   attr_accessible :price, :quantity, :references, :references, :puzzle_config_id
+  belongs_to :cart
+  belongs_to :puzzle_config
+
+  def title
+    puzzle_config.puzzle.to_s + ' :: ' + puzzle_config.to_s
+  end
+
 end
