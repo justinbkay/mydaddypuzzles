0bdf8fc7eff517a34d1e20793ed46e1aa0e19606
Justin <justinbkay@gmail.com>
2013-02-20 13:01:38 -0700

    fix typo

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
-  :address        => smtp.mailgun.org,
+  :address        => 'smtp.mailgun.org',
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'app12119805.mailgun.org',
